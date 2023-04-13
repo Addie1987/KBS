cc3829f5cc18de35b62cafa43339da500cd45cfa
Addie1987 <128349613+Addie1987@users.noreply.github.com>
2023-04-13 16:46:16 +0800

    Add files via upload

--- js/games.js
+++ js/games.js
@@ -1,6 +1,11 @@
-// games
-$("#easy-filter-wrap").easyFilter();
-$("#easy-filter-radio").easyFilter({
-  // firstFilter: "item12",
-  firstFilter: "*",
-});
+(function ($) {
+  $(function () {
+    $('#myExampleBestResponsiveTab1').laraGemsBestResponsiveTabs({
+      container: $('#myExampleTabsContent')
+    });
+    $('#myExampleBestResponsiveTab1').on('lgTabItem:toogle', function (event, previous_item, next_item) {
+      console.log('Previous item', previous_item);
+      console.log('Next item', next_item);
+    });
+  });
+})(jQuery);
